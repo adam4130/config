db83c5ed3163ed7c61f9d778b62f8be175993c0d
Adam Lee <adam4130@users.noreply.github.com>
2019-08-08 16:42:45 -0600

    Overhauled vimrc

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 CONFIGDIR="$(dirname "$(realpath $0)")"
 
 # GitHub
-ln -sf $CONFIGDIR/gitignore ~/.gitignore
-ln -sf $CONFIGDIR/gitconfig ~/.gitconfig
+ln -sf $CONFIGDIR/git/gitignore ~/.gitignore
+ln -sf $CONFIGDIR/git/gitconfig ~/.gitconfig
 
 # Vim
 mkdir -p ~/.vim/undodir
-mkdir -p ~/.vim/colors
-cp $CONFIGDIR/gruvbox.vim ~/.vim/colors
-ln -sf $CONFIGDIR/vimrc ~/.vimrc
+mkdir -p ~/.vim/spell
+ln -sf $CONFIGDIR/vim/vimrc ~/.vimrc
+ln -sf $CONFIGDIR/vim/en.utf-8.add ~/.vim/spell/en.utf-8.add
